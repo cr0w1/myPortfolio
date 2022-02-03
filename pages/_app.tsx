6994489fb89c7c9818ae23bdca73a4153854c1c1
cr0w1 <clebsonjose30@gmail.com>
2022-02-03 00:24:07 -0300

    implementing navbar and home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import theme from '../assets/configs/theme'
+import NavBar from './components/nav_bar/nav_bar'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ChakraProvider theme={theme}>
+            <NavBar/>
+            <Component {...pageProps} />
+        </ChakraProvider>
 }
 
 export default MyApp
