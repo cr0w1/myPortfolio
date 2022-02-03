9cdaabc9956f2d8a2077753913c38a363c81bb68
cr0w1 <clebsonjose30@gmail.com>
2022-02-03 00:26:36 -0300

    implementing navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,7 @@ const Home: NextPage = () => {
               textTransform='uppercase'
               fontSize='sm'
               letterSpacing='wide'
-              color='teal.600'
+              color='teal.600' 
             >
               Marketing
             </Text>
